392086b39a011d58912e093379dbfaffbf1aa997
Madan <madan.patakota@gmail.com>
2023-08-09 09:21:55 +0530

    Services Basics

---
+++ src/app/log.service.ts
@@ -0,0 +1,14 @@
+export class LogService {
+  /*Action | event | method | function */
+  /* Set of staments are executed at single time */
+  getLog() {
+    console.log('Its working!!!!!!');
+  }
+
+  getLogByName(name: string) {
+    console.log('You have entered the ' + name);
+  }
+
+
+
+}
